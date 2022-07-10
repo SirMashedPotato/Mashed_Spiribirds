7ac8c2b23a423d6a6a73044fc1285dead27fe1a8
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-07-11 11:28:38 +1200

    Bushes done

---
+++ 1.3/Source/Mashed_Spiribirds/Mashed_Spiribirds/PawnKindDefOf.cs
@@ -0,0 +1,20 @@
+﻿using Verse;
+using RimWorld;
+
+namespace Mashed_Spiribirds
+{
+    [DefOf]
+    public static class PawnKindDefOf
+    {
+        static PawnKindDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(PawnKindDefOf));
+        }
+
+        public static PawnKindDef Mashed_Spiribird_SpiribirdGreen;
+        public static PawnKindDef Mashed_Spiribird_SpiribirdOrange;
+        public static PawnKindDef Mashed_Spiribird_SpiribirdRed;
+        public static PawnKindDef Mashed_Spiribird_SpiribirdYellow;
+        public static PawnKindDef Mashed_Spiribird_SpiribirdPrism;
+    }
+}
---
+++ 1.3/Source/Mashed_Spiribirds/Mashed_Spiribirds/Plant_Spiribush.cs
@@ -0,0 +1,44 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+namespace Mashed_Spiribirds
+{
+    [StaticConstructorOnStartup]
+    public static class SpiribirdList
+    {
+        private static Dictionary<PawnKindDef, float> spiribirds = new Dictionary<PawnKindDef, float>();
+
+        static SpiribirdList()
+        {
+            spiribirds.Add(PawnKindDefOf.Mashed_Spiribird_SpiribirdGreen, 5f);
+            spiribirds.Add(PawnKindDefOf.Mashed_Spiribird_SpiribirdOrange, 5f);
+            spiribirds.Add(PawnKindDefOf.Mashed_Spiribird_SpiribirdRed, 5f);
+            spiribirds.Add(PawnKindDefOf.Mashed_Spiribird_SpiribirdYellow, 5f);
+            spiribirds.Add(PawnKindDefOf.Mashed_Spiribird_SpiribirdPrism, 1f);
+        }
+
+        public static PawnKindDef RandomKind()
+        {
+            return spiribirds.RandomElementByWeight(x=>x.Value).Key;
+        }
+    }
+
+    public class Plant_Spiribush : Plant
+    {
+
+        public override void PlantCollected(Pawn by)
+        {
+            if (this.HarvestableNow)
+            {
+                if (Rand.Chance(0.15f)) //could possibly be a setting
+                {
+                    Pawn newP = PawnGenerator.GeneratePawn(SpiribirdList.RandomKind(), null);
+                    GenSpawn.Spawn(newP, this.Position, this.Map);
+                }
+            }
+
+            base.PlantCollected(by);
+        }
+    }
+}
