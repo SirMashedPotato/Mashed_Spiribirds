264e4abdee4bfd311c1203a4d463c7192dce830b
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-08-07 16:52:43 +1200

    Refining + SeedsPlease Lite support

--- 1.3/Source/Mashed_Spiribirds/Mashed_Spiribirds/ThingComp/Comp_PetalaceStats.cs
+++ 1.3/Source/Mashed_Spiribirds/Mashed_Spiribirds/ThingComp/Comp_PetalaceStats.cs
@@ -1,6 +1,7 @@
 ﻿using RimWorld;
 using System.Collections.Generic;
 using Verse;
+using System.Linq;
 
 namespace Mashed_Spiribirds
 {
@@ -22,16 +23,26 @@ namespace Mashed_Spiribirds
                         "Mashed_Spiribird_PollenAffinity".Translate(pa.pollenHediff.label, pa.limit.ToStringPercent(), pa.severityGain.ToStringPercent(), actualSeverity.ToStringPercent()), 
                         0, null, null, false);
                 }
-                ///Spiribug molt affinity
-                if (props.spiribugKind != null)
+                ///Spiribug molt
+                if (LoadedModManager.RunningModsListForReading.Any(x => x.Name == "Mashed's Spiribugs" || x.PackageId == "SirMashedPotato.Spiribugs"))
                 {
-                    CompProperties_SpiribugMolt moltComp = props.spiribugKind.GetCompProperties<CompProperties_SpiribugMolt>();
-                    float bugQ = ((float)qc + 1 + props.extraMoltGain) / 3;
-                    float actualSeverity = moltComp.baseIncrease * bugQ;
-                    yield return new StatDrawEntry(StatCategoryDefOf.Mashed_Spiribird_PetalaceStats, props.spiribugKind.label, "",
-                        "Mashed_Spiribird_MoltAffinity".Translate(props.spiribugKind.label, actualSeverity.ToStringPercent()),
-                        0, null, null, false);
+                    float bugQ = ((float)qc + 1) / 3;
+                    float actualSeverity = 0.1f * bugQ;
+                    yield return new StatDrawEntry(StatCategoryDefOf.Mashed_Spiribird_PetalaceStats, "Mashed_Spiribird_MoltLabel".Translate(), "",
+                        "Mashed_Spiribird_MoltAffinity".Translate(actualSeverity.ToStringPercent()),
+                        0, null, null, false); 
+                    ///Spiribug molt affinity
+                    if (props.spiribugKind != null)
+                    {
+                        CompProperties_SpiribugMolt moltComp = props.spiribugKind.GetCompProperties<CompProperties_SpiribugMolt>();
+                        bugQ = ((float)qc + 1 + props.extraMoltGain) / 3;
+                        actualSeverity = moltComp.baseIncrease * bugQ;
+                        yield return new StatDrawEntry(StatCategoryDefOf.Mashed_Spiribird_PetalaceStats, props.spiribugKind.label, "",
+                            "Mashed_Spiribird_MoltAffinityBoosted".Translate(props.spiribugKind.label, props.extraMoltGain, actualSeverity.ToStringPercent()),
+                            0, null, null, false);
+                    }
                 }
+               
             }
         }
     }
