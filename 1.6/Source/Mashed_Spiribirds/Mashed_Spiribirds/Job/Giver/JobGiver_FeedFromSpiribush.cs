a8771d2731e8ed24c0c5b6130aed181bc4a54996
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-06-15 12:55:52 +1200

    only harvestable spiribush can be fed from

--- 1.6/Source/Mashed_Spiribirds/Mashed_Spiribirds/Job/Giver/JobGiver_FeedFromSpiribush.cs
+++ 1.6/Source/Mashed_Spiribirds/Mashed_Spiribirds/Job/Giver/JobGiver_FeedFromSpiribush.cs
@@ -1,4 +1,5 @@
-﻿using Verse;
+﻿using RimWorld;
+using Verse;
 using Verse.AI;
 
 namespace Mashed_Spiribirds
@@ -12,8 +13,17 @@ namespace Mashed_Spiribirds
 				return null;
 			}
 			Thing plant = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForDef(ThingDefOf.Mashed_Spiribird_Spiribush), PathEndMode.OnCell, 
-				TraverseParms.For(pawn, Danger.Deadly, TraverseMode.ByPawn, false), radius, null, null, 0, -1, false, RegionType.Set_Passable, false);
+				TraverseParms.For(pawn, Danger.Deadly, TraverseMode.ByPawn, false), radius, MatureValidator, null, 0, -1, false, RegionType.Set_Passable, false);
 			return plant == null ? null : JobMaker.MakeJob(JobDefOf.Mashed_Spiribirds_FeedFromSpiribush, plant);
+
+			bool MatureValidator(Thing t)
+			{
+				if (t is Plant p)
+				{
+					return p.HarvestableNow;
+				}
+				return false;
+			}
 		}
 
 		public float radius = 20f;
